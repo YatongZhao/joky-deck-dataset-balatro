5ab1ce3cfccb6ecc797f6cbd36ae04de1eae4a4f
yatongzhao <zyt768295828@163.com>
2025-04-14 16:30:55 +0800

    Set base path for Vite build configuration to '/joky-deck-dataset-balatro' and remove empty rollupOptions.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/joky-deck-dataset-balatro',
   build: {
     target: 'esnext',
-    rollupOptions: {
-      output: {
-      }
-    }
   }
 })
