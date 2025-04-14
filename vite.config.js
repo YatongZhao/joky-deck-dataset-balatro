c64d58a479c83a09e099bb602c357aae45912c5f
yatongzhao <zyt768295828@163.com>
2025-04-14 18:33:25 +0800

    Add rollupOptions to Vite configuration for custom chunk naming and manual chunking of config files.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ export default defineConfig({
   base: '/joky-deck-dataset-balatro',
   build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name].js',
+        manualChunks(id) {
+          if (id.includes('src/config/index.ts')) {
+            return 'config'
+          }
+        }
+      }
+    }
   }
 })
