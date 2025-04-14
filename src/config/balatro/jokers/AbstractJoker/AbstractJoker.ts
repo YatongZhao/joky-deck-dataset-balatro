65d433835ccac3e421fec5f06e225b344f5a1f89
yatongzhao <zyt768295828@163.com>
2025-04-14 22:18:40 +0800

    Update AbstractJoker to dynamically reflect the current number of Joker cards in the description and adjust the multiplier calculation accordingly.

--- src/config/balatro/jokers/AbstractJoker/AbstractJoker.ts
+++ src/config/balatro/jokers/AbstractJoker/AbstractJoker.ts
@@ -6,23 +6,28 @@ import jokerJokerImage from './joker.png'
 export const getAbstractJokerConfig = ({ jokerCard, jokerCategory, style }: GetConfigParam): JokerConfig => {
   const ABSTRACT_JOKER_METADATA: JokerMetadata = {
     price: 4,
-    name: '抽象小丑',
-    description: '每张小丑牌+3倍率',
+    name: 'Abstract Joker',
+    description: '+3 Mult for each Joker card (Currently +0 Mult)',
     image: style === 'flat' ? flatJokerImage : jokerJokerImage,
     category: jokerCategory.common,
   }
 
   class AbstractJoker extends jokerCard {
+    private jokerCount = 0;
     constructor(game: Game) {
       super({
         game: game,
         ...ABSTRACT_JOKER_METADATA,
       });
+
+      this.game.handJokerCards.value$.subscribe(jokerCards => {
+        this.jokerCount = jokerCards.length;
+        this.description.setValue(`+3 Mult for each Joker card (Currently +${this.jokerCount * 3} Mult)`);
+      });
     }
 
     async afterAllCardsEmitHook() {
-      const jokerCount = this.game.handJokerCards.value.length;
-      this.game.round.value.handMultiFactor.setValue(factor => factor + jokerCount * 3);
+      this.game.round.value.handMultiFactor.setValue(factor => factor + this.jokerCount * 3);
       await this.emitEffect.emit();
     }
   }
